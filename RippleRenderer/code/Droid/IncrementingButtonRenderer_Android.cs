3a46f239300137da364ee0d64345e4c1cd2bf36c
Matt Hoffman <hofmadresu@gmail.com>
2018-04-06 09:51:45 -0400

    finish initial renderer

--- RippleRenderer/code/Droid/IncrementingButtonRenderer_Android.cs
+++ RippleRenderer/code/Droid/IncrementingButtonRenderer_Android.cs
@@ -1,21 +1,46 @@
-﻿using System;
+﻿using System.ComponentModel;
 using Android.Content;
+using Android.Views;
+using Android.Widget;
+using RippleRenderer;
+using RippleRenderer.Droid;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportRenderer(typeof(IncrementingButton), typeof(IncrementingButtonRenderer_Android))]
 namespace RippleRenderer.Droid
 {
 	public class IncrementingButtonRenderer_Android : ViewRenderer<IncrementingButton, Android.Views.View>
     {
 		private readonly Context _context;
 
-		protected IncrementingButtonRenderer_Android(Context context) : base(context)
+		public IncrementingButtonRenderer_Android(Context context) : base(context)
 		{
 			_context = context;
 		}
 
-		protected override void OnElementChanged(ElementChangedEventArgs<IncrementingButton> e)
+        private TextView _buttonTitleView;
+        private TextView _clickCountView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<IncrementingButton> e)
 		{
 			base.OnElementChanged(e);
-		}
+
+            if (Control == null)
+            {
+                // Inflate the IncrementingButton layout
+                var inflater = _context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+                var rootLayout = inflater.Inflate(Resource.Layout.IncrementingButton, null, false);
+
+                // Set text on Textviews
+                _buttonTitleView = rootLayout.FindViewById<TextView>(Resource.Id.ButtonTitle);
+                _buttonTitleView.Text = Element.Title;
+                _clickCountView = rootLayout.FindViewById<TextView>(Resource.Id.ClickCount);
+                _clickCountView.Text = $"Clicked {Element.ClickCount} times";
+
+                // Tell Xamarin to user our layout for the control
+                SetNativeControl(rootLayout);
+            }
+        }
 	}
 }
